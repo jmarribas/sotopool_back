69e5745edade3762f19846503b86baada85ce6a7
jmarribas <jomuarribas@gmail.com>
2024-06-09 18:20:18 +0200

    code generator

--- utils/codeGenerator.js
+++ utils/codeGenerator.js
@@ -3,19 +3,30 @@ const fs = require('fs');
 const path = require('path');
 
 const getCode = () => {
-  return Math.floor(1000 + Math.random() * 9000);
+  return Math.floor(1000 + Math.random() * 9000).toString();
+}
+
+const createDirectory = (dirPath) => {
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath, { recursive: true });
+  }
 }
 
 const generateAndSaveCode = () => {
-  const code = getCode();
-  fs.writeFileSync(path.join("src/code", 'dailyCode.txt'), code);
+  const dirPath = path.join(__dirname, '/code');
+  createDirectory(dirPath);
+  const code = getCode().toString();
+  const filePath = path.join(dirPath, 'dailyCode.txt');
+  fs.writeFileSync(filePath, code, 'utf8');
+  console.log(`Código generado: ${code}`);
 }
 
-cron.schedule('0 10 * * *', generateAndSaveCode);
+cron.schedule('* * * * *', generateAndSaveCode);
 
 const getCodeFromFile = () => {
   try {
-    return fs.readFileSync(path.join("src/code", 'dailyCode.txt'), 'utf8');
+    const filePath = path.join(__dirname, '/code', 'dailyCode.txt');
+    return fs.readFileSync(filePath, 'utf8');
   } catch (err) {
     console.error('No se pudo leer el código del archivo', err);
     return null;
